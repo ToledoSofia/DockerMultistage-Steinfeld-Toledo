c68e820121ed46997fe3dea9e806ec90bf9f8792
toledosofia <alumno24.toledo.sofia@ipm.edu.ar>
2024-05-19 19:47:45 -0300

    Dockerfiles y pag estatica

---
+++ dockerMulti/script.js
@@ -0,0 +1,7 @@
+function btn(){
+    if(document.getElementById("hola").style.color == "green"){
+        document.getElementById("hola").style.color = "red";
+    }else{
+        document.getElementById("hola").style.color = "green";
+    }
+}
